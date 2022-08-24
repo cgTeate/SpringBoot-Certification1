a77f71491e0dbfaf53f54216d170370a372865f2
Calvin Teater <laughingglint3@gmail.com>
2022-08-24 14:36:31 -0500

    basic spring boot project setup

---
+++ src/main/java/com/calvinteater/springtest1/firstController.java
@@ -0,0 +1,13 @@
+package com.calvinteater.springtest1;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class firstController {
+
+    @GetMapping("/welcome")
+    public String first_api(){
+        return  "Welcome to the first_api Project";
+    }
+}
