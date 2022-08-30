dccfd281f684b01f3e6bbb9390a5077821b70d58
Calvin Teater <laughingglint3@gmail.com>
2022-08-30 08:48:48 -0500

    map Student class to database

--- src/main/java/com/calvinteater/springtest1/model/Student.java
+++ src/main/java/com/calvinteater/springtest1/model/Student.java
@@ -2,6 +2,13 @@ package com.calvinteater.springtest1.model;
 
 import java.time.LocalDate;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
+
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -13,7 +20,19 @@ import lombok.ToString;
 @Getter
 @Setter
 @ToString
+@Entity
+@Table
 public class Student {
+    @Id
+    @SequenceGenerator(
+        name= "student_sequence",
+        sequenceName= "student_sequence",
+        allocationSize = 1
+    )
+    @GeneratedValue(
+        strategy = GenerationType.SEQUENCE,
+        generator = "student_sequence"
+    )
     private Long id;
     private String name;
     private String email;
