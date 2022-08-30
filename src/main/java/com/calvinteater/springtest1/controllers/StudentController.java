31e6bde36d91c1e486ad61d367723f2591ba0e8e
Calvin Teater <laughingglint3@gmail.com>
2022-08-29 20:44:04 -0500

    add dependency injection

--- src/main/java/com/calvinteater/springtest1/controllers/StudentController.java
+++ src/main/java/com/calvinteater/springtest1/controllers/StudentController.java
@@ -4,6 +4,7 @@ package com.calvinteater.springtest1.controllers;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,6 +18,7 @@ public class StudentController {
 
     private final StudentService studentService;
 
+    @Autowired
     public StudentController(StudentService studentService) {
         this.studentService = studentService;
     }
--- src/main/java/com/calvinteater/springtest1/service/StudentService.java
+++ src/main/java/com/calvinteater/springtest1/service/StudentService.java
@@ -4,8 +4,11 @@ import java.time.LocalDate;
 import java.time.Month;
 import java.util.List;
 
+import org.springframework.stereotype.Service;
+
 import com.calvinteater.springtest1.model.Student;
 
+@Service
 public class StudentService {
     public List<Student> getStudents(){
         return List.of(
